ee6a331de26bbda2403c611efb7ec65f33613afa
Sam <sejohnson2020@outlook.com>
2024-06-29 14:37:37 -0400

    TimesFM test results

--- timesfm_test.sh
+++ timesfm_test.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 python -m src.actions.evaluate --dataset_directory data/high_var_oct16/test --results_name high_var_timesfm --ablation_lens 15 30 90 --units s --model timesfm
 python -m src.actions.evaluate --dataset_directory data/min_av/amatrol-Mar24/test_CNC --results_name CNC_timesfm --ablation_lens 15 30 90 --units min --model timesfm
 python -m src.actions.evaluate --dataset_directory data/min_av/OptoMMP-Oct23/test_M00 --results_name M00_timesfm --ablation_lens 15 30 90 --units min --model timesfm
